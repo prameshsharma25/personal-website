c3784095555fb09caafde0d7e0c4ab04a626105d
Pramesh Sharma <prameshsharma@Prameshs-MacBook-Air.local>
2026-04-04 15:22:59 -0400

    added google ad sense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2651199811728382"
+        crossOrigin="anonymous"
+        strategy="beforeInteractive"
+      />
       <Navbar />
       <Component {...pageProps} />
     </>
