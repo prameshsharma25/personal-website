87abb9953e024628293af9f06980feca6a306a10
Pramesh Sharma <prameshsharma@Prameshs-MacBook-Air.local>
2026-04-04 15:26:29 -0400

    added npm analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,6 +15,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       />
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
