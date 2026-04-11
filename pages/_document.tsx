e81a878cc00c749d747d214b0cd661c3cfac823b
Pramesh Sharma <prameshsharma@Prameshs-MacBook-Air.local>
2026-04-11 19:32:14 -0400

    add SEO optimizations

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <Script
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2651199811728382"
             crossOrigin="anonymous"
-            strategy="beforeInteractive"
-          />
+          ></script>
         </Head>
         <body>
           <Main />
