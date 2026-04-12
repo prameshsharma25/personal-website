c6266d0f704d05fa6e58965f077794e6275cf6fb
Pramesh Sharma <prameshsharma@Prameshs-MacBook-Air.local>
2026-04-12 15:51:52 -0400

    added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <script
             async
